45cd5de909994b6ed3edc48f792d13c7c3212658
Maurits van der Schee <maurits@vdschee.nl>
2022-08-26 22:19:47 +0200

    update

--- Controllers/Todo.vb
+++ Controllers/Todo.vb
@@ -6,31 +6,27 @@ Imports Maussoft.Mvc
 Namespace Controllers
 	Public Class Todo
 		
-	Public Sub Add(ByVal context As WebContext(Of Session))
-        If context.Method = "POST" Then
-            context.Data.Values = New ViewData(context.Post)
-            context.Data.Errors = New ViewData()
-
-            If context.Data.Values.Item.Length = 0 Then
-                context.Data.Errors.Item = "Field is mandatory"
+        Public Sub Add(ByVal context As WebContext(Of Session))
+            context.Data.Title = "Todo app"
+            If context.Method = "POST" Then
+                context.Data.Values = New ViewData(context.Post)
+                context.Data.Errors = New ViewData()
+                If context.Data.Values.Item.Length = 0 Then
+                    context.Data.Errors.Item = "Field is mandatory"
+                End If
+                If context.Data.Errors.Empty() Then
+                    context.Session.Items.Add(context.Data.Values.Item)
+                    context.Redirect("/Todo")
+                    Return
+                End If
+            Else
+                context.Data.Values = New ViewData()
             End If
+        End Sub
 
-            If context.Data.Errors.Empty() Then
-                context.Session.Items.Add(context.Data.Values.Item)
-                context.Redirect("/Todo")
-                Return
-            End If
-        Else
-            context.Data.Values = New ViewData()
-            context.Data.Values.Item = ""
-        End If
-
-        context.Data.Title = "Todo app"
-    End Sub
-
-    Public Sub Index(ByVal context As WebContext(Of Session))
-        context.Data.Title = "Todo app"
-    End Sub
+        Public Sub Index(ByVal context As WebContext(Of Session))
+            context.Data.Title = "Todo app"
+        End Sub
 
 	End Class
 
